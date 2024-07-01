88d5126df2e60e4475d4bb9f0084c4a765407db1
João Vitor Araújo de Jesus <joaovitoraj@poli.ufrj.br>
2024-07-01 02:30:38 -0300

    features index.html

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -55,13 +55,14 @@ $( document ).ready(function() {
 
     var curActive = $('.side-nav').find('.is-active'),
         curPos = $('.side-nav').children().index(curActive),
-        lastItem = $('.side-nav').children().length - 1,
-        nextPos = lastItem;
-
-    updateNavs(lastItem);
+        lastItem = $('.side-nav').children().length -1,
+        nextPos = lastItem; 
+  
+    updateNavs(nextPos);
     updateContent(curPos, nextPos, lastItem);
-
+  
   });
+  
 
   // swipe support for touch devices
   var targetElement = document.getElementById('viewport'),
